60aa068dbcfbc26447dec00feab8734b12ea7770
LeinneSW <nno88551@naver.com>
2024-03-27 09:23:06 +0900

    findRepeatedText의 잘못된 로직 수정, 봇 예외 추가

--- public/tts/audio.js
+++ public/tts/audio.js
@@ -3,15 +3,14 @@ let wsURL, ttsURL
 const playList = [] // Audio[]
 
 const findRepeatedText = (str) => {
-    const len = Math.ceil(str.length / 3)
-    for(let i = 1; i <= len; ++i){// 문자열의 길이의 1/3 까지만 확인(4회이상 반복시를 판단하기 위해서임)
+    const len = Math.ceil(str.length / 4)
+    for(let i = 1; i <= len; ++i){ // 문자열의 길이의 1/4 까지만 확인(4회이상 반복시를 판단하기 위해서임)
+        let index = 0, count = 1
         const substring = str.substring(0, i)
-        let index = 0, count = 0
         while((index = str.indexOf(substring, index + i)) !== -1){
             ++count;
         }
-        console.log(`count: ${count}, substring: ${substring}`)
-        if(count > 1 && count * substring.length === len){
+        if(count > 3 && count * substring.length === str.length){
             return {substring, count};
         }
     }
@@ -19,7 +18,7 @@ const findRepeatedText = (str) => {
 }
 
 const playTTS = (text) => {
-    const sound = new Audio(ttsURL.replaceAll('${text}' ,encodeURIComponent(text)))
+    const sound = new Audio(ttsURL.replaceAll('${text}', encodeURIComponent(text)))
     sound.onended = () => {
         console.log('재생완료:', text)
         playList.splice(playList.indexOf(sound), 1);
@@ -42,11 +41,15 @@ const connect = () => {
     client.onmessage = e => {
         try{
             const json = JSON.parse(e.data.toString())
+            if(json.user.nickname.endsWith('봇')){
+                return
+            }
+
             const repeatData = findRepeatedText(json.message)
             if(repeatData){
-                playTTS(repeatData.substring.repeat(repeatData.substring.length === 1 ? Math.min(count, 8) : Math.min(count, 3)))
+                const {substring, count} = repeatData
+                playTTS(substring.repeat(substring.length < 3 ? Math.min(count, 8) : 3))
             }else{
-                console.log('실패...')
                 playTTS(json.message)
             }
         }catch{}
