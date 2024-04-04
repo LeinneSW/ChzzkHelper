2d6d46ed172dbd11ca974c2029e1194e1b67cc6f
LeinneSW <nno88551@naver.com>
2024-04-04 17:25:25 +0900

    UI 개선: 투표 alert 창을 모두 alert_modal로 변경

--- public/vote/js/socket.js
+++ public/vote/js/socket.js
@@ -12,12 +12,12 @@ const startVote = (event) => {
 
     const elements = document.querySelectorAll('ol > li > input')
     if(elements.length < 2){
-        window.api.alert('투표할 항목을 입력해주세요')
+        showAlertModal('항목 없음', '투표할 항목을 입력해주세요')
         return
     }
 
     window.onbeforeunload = () => {
-        window.api.alert('투표가 진행중입니다. 다른화면으로 이동하려면 투표를 마감해주세요.', '투표 진행중')
+        showAlertModal('투표 진행중', '투표가 진행중입니다. 다른화면으로 이동하려면 투표를 마감해주세요.')
         return ''
     }
 
