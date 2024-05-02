1e10d7729229d6e49b0c583149b5eea514978602
LeinneSW <nno88551@naver.com>
2024-05-02 11:01:51 +0900

    UI 추가: 설정의 세부설명을 보여주는 기능 추가

--- public/chatting/js/setting.js
+++ public/chatting/js/setting.js
@@ -4,6 +4,32 @@ const setInputValue = (input, suffix = '') => {
     customCss && document.documentElement.style.setProperty(customCss, input.value + 'px')
 }
 
+const showTooltip = (option) => {
+    if(option.nextElementSibling.classList.contains('tooltip')){
+        // 모종의 이유로 툴팁이 삭제되지 않은경우
+        return
+    }
+
+    const description = option.dataset.description
+    if(!description){
+        return
+    }
+
+
+    const tooltip = document.createElement('div')
+    tooltip.className = 'tooltip'
+    tooltip.innerHTML = description.replaceAll('\\n', '<br>')
+    option.parentNode.insertBefore(tooltip, option.nextElementSibling)
+    tooltip.style.top = (option.getBoundingClientRect().top - tooltip.offsetHeight - 12) + 'px'
+}
+
+const hideTooltip = (option) => {
+    const tooltip = option.nextElementSibling
+    if(tooltip.classList.contains('tooltip')){
+        tooltip.remove()
+    }
+}
+
 window.addEventListener('load', () => {
     const settings = document.getElementById('settings')
     document.documentElement.addEventListener('mouseenter', () => settings.classList.add('show'))
@@ -28,5 +54,10 @@ window.addEventListener('load', () => {
             const saveName = radio.dataset.saveName
             saveName && localStorage.setItem(saveName, radio.value + '')
         })
+    }
+    const optionList = document.querySelectorAll(`#settings > .option-title`)
+    for(const option of optionList){
+        option.addEventListener('mouseenter', () => showTooltip(option))
+        option.addEventListener('mouseleave', () => hideTooltip(option))
     }
 })
